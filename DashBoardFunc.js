5b5e282d4ca6acd478d7de5a6f9d2f72fa20b438
jeevan raj <jeevan.sr@lnttechservices.com>
2019-07-01 21:55:09 +0530

    LIFE PORTAL WEBSITE.
    1.Side Bar working fine.
    2.Drop down working fine.
    3.Navigation tabs working fine.
    
    files:
     DashBoard.html
     DashBoardFunc.js
     DashBoardStyle.css

---
+++ DashBoardFunc.js
@@ -0,0 +1,10 @@
+function w3_open() {
+  document.getElementById("main").style.marginLeft = "25%";
+  document.getElementById("mySidebar").style.width = "25%";
+  document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("openNav").style.display = 'none';
+}
+
+function toggleSidebar() {
+  document.getElementById("sideBar").style.display = "block";
+}
