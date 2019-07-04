a21f47be47fe62a3299c5cbb7e14915ce9a10cf9
jeevan raj <jeevan.sr@lnttechservices.com>
2019-07-04 20:08:31 +0530

    LIFE PORTAL WEBSITE.
    my second commit.
    
    1.when click on dashboard icon navigation bar opens and click anywhere
      to close navigation bar.
    
    2.the above function also works for drop down menu.
    
    3.created divisions for all the tabs separately and when clicked on each
      tab, the respected divisions get opens.
    
    files modified:
    		 DashBoard.html
    		 DashBoardFunc.js
    		 DashBoardStyle.css

--- DashBoardFunc.js
+++ DashBoardFunc.js
@@ -1,10 +1,110 @@
-function w3_open() {
-  document.getElementById("main").style.marginLeft = "25%";
-  document.getElementById("mySidebar").style.width = "25%";
-  document.getElementById("mySidebar").style.display = "block";
-  document.getElementById("openNav").style.display = 'none';
+function Sidebar() {
+  document.getElementById("sidebar").style.display = "block";
+}
+function dropDown() {
+  document.getElementById("dropdownmenu").style.display = "block";
+}
+function Close(){
+  document.getElementById("dropdownmenu").style.display = "none";
+  document.getElementById("sidebar").style.display = "none";
 }
 
-function toggleSidebar() {
-  document.getElementById("sideBar").style.display = "block";
+function myTrainingFunction(){
+  document.getElementById('myTrainingDivision').style.display = 'block';
+  document.getElementById('requestedTrainingDivision').style.display = 'none';
+  document.getElementById('trainingCatalogDivision').style.display = 'none';
+  document.getElementById('myLearningPlansDivision').style.display = 'none';
+  document.getElementById('atlDivision').style.display = 'none';
+  document.getElementById('idpDivision').style.display = 'none';
+  document.getElementById('myCompletionsDivision').style.display = 'none';
+  document.getElementById('myOnlineTestsDivision').style.display = 'none';
+  document.getElementById('moocCoursesDivision').style.display = 'none';
+}
+function requestedTrainingFunction() {
+  document.getElementById('myTrainingDivision').style.display = 'none';
+  document.getElementById('requestedTrainingDivision').style.display = 'block';
+  document.getElementById('trainingCatalogDivision').style.display = 'none';
+  document.getElementById('myLearningPlansDivision').style.display = 'none';
+  document.getElementById('atlDivision').style.display = 'none';
+  document.getElementById('idpDivision').style.display = 'none';
+  document.getElementById('myCompletionsDivision').style.display = 'none';
+  document.getElementById('myOnlineTestsDivision').style.display = 'none';
+  document.getElementById('moocCoursesDivision').style.display = 'none';
+}
+function trainingCatalogFunction() {
+  document.getElementById('myTrainingDivision').style.display = 'none';
+  document.getElementById('requestedTrainingDivision').style.display = 'none';
+  document.getElementById('trainingCatalogDivision').style.display = 'block';
+  document.getElementById('myLearningPlansDivision').style.display = 'none';
+  document.getElementById('atlDivision').style.display = 'none';
+  document.getElementById('idpDivision').style.display = 'none';
+  document.getElementById('myCompletionsDivision').style.display = 'none';
+  document.getElementById('myOnlineTestsDivision').style.display = 'none';
+  document.getElementById('moocCoursesDivision').style.display = 'none';
+}
+function myLearningPlansFunction() {
+  document.getElementById('myTrainingDivision').style.display = 'none';
+  document.getElementById('requestedTrainingDivision').style.display = 'none';
+  document.getElementById('trainingCatalogDivision').style.display = 'none';
+  document.getElementById('myLearningPlansDivision').style.display = 'block';
+  document.getElementById('atlDivision').style.display = 'none';
+  document.getElementById('idpDivision').style.display = 'none';
+  document.getElementById('myCompletionsDivision').style.display = 'none';
+  document.getElementById('myOnlineTestsDivision').style.display = 'none';
+  document.getElementById('moocCoursesDivision').style.display = 'none';
+}
+function atlFunction() {
+  document.getElementById('myTrainingDivision').style.display = 'none';
+  document.getElementById('requestedTrainingDivision').style.display = 'none';
+  document.getElementById('trainingCatalogDivision').style.display = 'none';
+  document.getElementById('myLearningPlansDivision').style.display = 'none';
+  document.getElementById('atlDivision').style.display = 'block';
+  document.getElementById('idpDivision').style.display = 'none';
+  document.getElementById('myCompletionsDivision').style.display = 'none';
+  document.getElementById('myOnlineTestsDivision').style.display = 'none';
+  document.getElementById('moocCoursesDivision').style.display = 'none';
+}
+function idpFunction() {
+  document.getElementById('myTrainingDivision').style.display = 'none';
+  document.getElementById('requestedTrainingDivision').style.display = 'none';
+  document.getElementById('trainingCatalogDivision').style.display = 'none';
+  document.getElementById('myLearningPlansDivision').style.display = 'none';
+  document.getElementById('atlDivision').style.display = 'none';
+  document.getElementById('idpDivision').style.display = 'block';
+  document.getElementById('myCompletionsDivision').style.display = 'none';
+  document.getElementById('myOnlineTestsDivision').style.display = 'none';
+  document.getElementById('moocCoursesDivision').style.display = 'none';
+}
+function myCompletionsFunction() {
+  document.getElementById('myTrainingDivision').style.display = 'none';
+  document.getElementById('requestedTrainingDivision').style.display = 'none';
+  document.getElementById('trainingCatalogDivision').style.display = 'none';
+  document.getElementById('myLearningPlansDivision').style.display = 'none';
+  document.getElementById('atlDivision').style.display = 'none';
+  document.getElementById('idpDivision').style.display = 'none';
+  document.getElementById('myCompletionsDivision').style.display = 'block';
+  document.getElementById('myOnlineTestsDivision').style.display = 'none';
+  document.getElementById('moocCoursesDivision').style.display = 'none';
+}
+function onlineTestsFunction() {
+  document.getElementById('myTrainingDivision').style.display = 'none';
+  document.getElementById('requestedTrainingDivision').style.display = 'none';
+  document.getElementById('trainingCatalogDivision').style.display = 'none';
+  document.getElementById('myLearningPlansDivision').style.display = 'none';
+  document.getElementById('atlDivision').style.display = 'none';
+  document.getElementById('idpDivision').style.display = 'none';
+  document.getElementById('myCompletionsDivision').style.display = 'none';
+  document.getElementById('myOnlineTestsDivision').style.display = 'block';
+  document.getElementById('moocCoursesDivision').style.display = 'none';
+}
+function moocCoursesFunction() {
+  document.getElementById('myTrainingDivision').style.display = 'none';
+  document.getElementById('requestedTrainingDivision').style.display = 'none';
+  document.getElementById('trainingCatalogDivision').style.display = 'none';
+  document.getElementById('myLearningPlansDivision').style.display = 'none';
+  document.getElementById('atlDivision').style.display = 'none';
+  document.getElementById('idpDivision').style.display = 'none';
+  document.getElementById('myCompletionsDivision').style.display = 'none';
+  document.getElementById('myOnlineTestsDivision').style.display = 'none';
+  document.getElementById('moocCoursesDivision').style.display = 'block';
 }
